dab43e44f53e30430073df7848bd4fd28da8c2e9
409570157 <lovepatty57@gmail.com>
2021-03-11 16:16:51 +0800

    Second

--- src/com/yuni/Customer.java
+++ src/com/yuni/Customer.java
@@ -4,16 +4,16 @@ public class Customer {
     int customer;
     int consumption;
     int discount;
+    int total;
 
     public Customer(int customer,int consumption,int discount,int total){
         this.customer = customer;
         this.consumption = consumption;
         this.discount = discount;
+        this.total = total;
     }
-    public  int total() {
-        return consumption-discount;
-    }
+
     public void print(){
-        System.out.println(customer +"\t"+consumption+"\t"+discount+"\t"+total());
+        System.out.println(customer +"\t"+consumption+"\t"+discount+"\t"+total);
     }
 }
--- src/com/yuni/List.java
+++ src/com/yuni/List.java
@@ -2,7 +2,15 @@ package com.yuni;
 
 public class List{
         public static void main(String[] args) {
-
+                Customer[] customers = {
+                        new Customer(13,490,0,490),
+                        new Customer(52,1000,100,900),
+                        new Customer(81,290,0, 290),
+                        new Customer(2122, 2000, 200, 1800)
+                };
+                for (int i = 0; i <customers.length; i++) {
+                        customers[i].print();
+                }
 
 
 
